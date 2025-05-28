3f26a2d857c23e6b982a579b505b11b62af45fde
Pese <146886140+Pesepvtltd@users.noreply.github.com>
2025-05-28 14:30:02 +0530

    Update the cors

--- server/server.js
+++ server/server.js
@@ -6,19 +6,35 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 
-
-
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ai-girl-chat.netlify.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes); 
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
