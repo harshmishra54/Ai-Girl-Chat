271a6e4fe300be79339c19a401ac8d224d1e56a2
Pese <146886140+Pesepvtltd@users.noreply.github.com>
2025-05-29 14:42:27 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
+// import Header from "./components/Header";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import RefundPolicy from "./pages/RefundPolicy";
+import ContactUs from "./pages/ContactUs";
 
 const App = () => {
   return (
@@ -14,6 +19,10 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsAndConditions />} />
+        <Route path="/refund" element={<RefundPolicy />} />
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
   );
