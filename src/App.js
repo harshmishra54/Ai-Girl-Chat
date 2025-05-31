61a14d55486d092e412c36c9aae3c78b8fde3707
Pese <146886140+Pesepvtltd@users.noreply.github.com>
2025-05-31 20:27:48 +0530

    update the app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Chat from "./pages/Chat";
+// import Chat from "./pages/Chat";
 // import Header from "./components/Header";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import RefundPolicy from "./pages/RefundPolicy";
+import BotPage from "./pages/BotPage";
 import ContactUs from "./pages/ContactUs";
 
 const App = () => {
@@ -18,11 +19,13 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {/* <Route path="/chat" element={<Chat />} /> */}
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsAndConditions />} />
         <Route path="/refund" element={<RefundPolicy />} />
         <Route path="/contact" element={<ContactUs />} />
+        
+<Route path="/bot" element={<BotPage />} />
       </Routes>
     </Router>
   );
